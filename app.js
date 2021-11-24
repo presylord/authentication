f51307de95cf4089336694132ddce835dd51242a
presylord <kidlat123@gmail.com>
2021-11-24 16:25:29 +0800

    hash

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 // default requirements
-require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-const encrypt = require("mongoose-encryption");
-
+const md5 = require("md5");
 const app = express();
 
 app.use(express.static("public"));
@@ -16,11 +14,6 @@ mongoose.connect("mongodb://localhost:27017/userDatabase");
 const userSchema = new mongoose.Schema({
   email: "string",
   password: "string",
-});
-
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"],
 });
 
 const User = mongoose.model("User", userSchema);
@@ -43,7 +36,7 @@ app.post("/login", function (req, res) {
     },
     function (err, foundUser) {
       if (foundUser) {
-        if (foundUser.password === req.body.password) {
+        if (foundUser.password === md5(req.body.password)) {
           console.log("Login Successful");
           res.render("secrets");
         } else {
@@ -66,7 +59,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
   newUser.save(function (err) {
     if (!err) {
